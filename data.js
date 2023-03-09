19c9c9cca1c6a2f40bf605eed83fa079ae0c250b
ikyaxen <125803227+ikyaxen@users.noreply.github.com>
2023-03-09 20:10:19 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,75 @@
+const coverDir = "./assets/cover";
+const audioDir = "./assets/songs";
+
+const songs = [
+  {
+    title: "Ghost",
+    artist: "Justin Beiber",
+    album: "Justice",
+    cover: `${coverDir}/justice.jpg`,
+    audioDir: `${audioDir}/ghost.mp3`,
+  },
+  {
+    title: "Sunflower",
+    artist: "Post Malone, Swae Lee",
+    album: "Unknow",
+    cover: `${coverDir}/sunflower.jpg`,
+    audioDir: `${audioDir}/sunflower.mp3`,
+  },
+  {
+    title: "abcdefu",
+    artist: "Gayle",
+    album: "Unknow",
+    cover: `${coverDir}/default.jpg`,
+    audioDir: `${audioDir}/abcdefu.mp3`,
+  },
+  {
+    title: "Build a bitch",
+    artist: "Bella Poarch",
+    album: "Unknow",
+    cover: `${coverDir}/build_a_bitch.jpg`,
+    audioDir: `${audioDir}/build_a_bitch.mp3`,
+  },
+  {
+    title: "good 4 u",
+    artist: "Olivia Rodrigo",
+    album: "SOUR",
+    cover: `${coverDir}/sour.jpg`,
+    audioDir: `${audioDir}/good_4_u.mp3`,
+  },
+  {
+    title: "Drivers License",
+    artist: "Olivia Rodrigo",
+    album: "SOUR",
+    cover: `${coverDir}/sour.jpg`,
+    audioDir: `${audioDir}/driver_license.mp3`,
+  },
+  {
+    title: "On The Ground",
+    artist: "Rose",
+    album: "Unknow",
+    cover: `${coverDir}/on_the_ground.jpg`,
+    audioDir: `${audioDir}/on_the_ground.mp3`,
+  },
+  {
+    title: "Peaches",
+    artist: "Justin Beiber",
+    album: "Justice",
+    cover: `${coverDir}/justice.jpg`,
+    audioDir: `${audioDir}/peaches.mp3`,
+  },
+  {
+    title: "Stay",
+    artist: "Justin Bieber, The Kid LAROI",
+    album: "Unknow",
+    cover: `${coverDir}/stay.jpg`,
+    audioDir: `${audioDir}/stay.mp3`,
+  },
+  {
+    title: "Tie Me Down",
+    artist: "Gryffin",
+    album: "Unknow",
+    cover: `${coverDir}/tie_me_down.jpg`,
+    audioDir: `${audioDir}/tie_me_down.mp3`,
+  },
+];
---
+++ script.js
@@ -0,0 +1,280 @@
+const musicContainer = document.getElementById("music_container");
+
+const modeButton = document.getElementById("music_mode_btn");
+const playButton = document.getElementById("music_play_btn");
+const previousButton = document.getElementById("previous_btn");
+const nextButton = document.getElementById("next_btn");
+const listButton = document.getElementById("songs_list_btn");
+
+const prgressArea = document.getElementById("prgress_area");
+const progressBar = document.getElementById("progress_bar");
+
+const currentTime = document.getElementById("current_time");
+const duration = document.getElementById("duration");
+
+const audio = document.getElementById("audio");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const cover = document.getElementById("cover");
+
+const songsListContainer = document.getElementById("songs_list_container");
+const songsListUlTag = songsListContainer.querySelector("ul");
+const songsListCloseButton = document.getElementById("song_list_close_btn");
+const songsListModeButton = document.getElementById("songs_list_mode_btn");
+const songsListModeName = document.getElementById("songs_list_mode_name");
+const modes = ["repeat", "repeat_one", "shuffle"];
+
+var music_index = 0;
+var mode_index = 0;
+var pasused = true;
+
+const setMusicContainerCenter = () => {
+  if (musicContainer.clientHeight > document.body.clientHeight) {
+    document.body.style.height = "fit-content";
+    // document.body.classList.remove("h-screen");
+    // document.body.classList.add("h-fit");
+  } else {
+    document.body.style.height = "100vh";
+    // document.body.classList.remove("h-fit");
+    // document.body.classList.add("h-screen");
+  }
+}
+
+const loadSongsList = () => {
+  songs.map((song, index) => {
+    let list_num = index + 1;
+    if (list_num < 10) {
+      list_num = '0' + list_num;
+    }
+    let liTag = `
+    <li index="${index}" list-number="${list_num}" onclick="playMusic(${index})" class="flex items-center hover:bg-gray-800 p-5 max-sm:p-3 rounded-xl cursor-pointer gap-5 overflow-hidden">
+      <div class="animation${index} flex items-center text-sm w-4">
+        <div>${list_num}</div>
+      </div>
+      <div class="flex gap-5 max-sm:gap-3 items-center">
+        <div class="w-max">
+          <img class="w-12 h-12 rounded-lg aspect-square object-cover shrink-0" src="${song.cover}" alt="${song.title}" />
+        </div>
+        <div>
+          <h1 class="text-base font-light">${song.title}</h1>
+          <div class="flex gap-3 text-sm text-slate-500 whitespace-nowrap overflow-hidden">
+            <h1>${song.artist}</h1>
+          </div>
+        </div>
+      </div>
+    </li>`;
+    songsListUlTag.insertAdjacentHTML("beforeend", liTag);
+  });
+}
+
+const loadPlayingSongInList = () => {
+  songsListUlTag.querySelectorAll('li').forEach(element => {
+    let index = element.getAttribute("index");
+    let number = element.getAttribute("list-number");
+
+    let animationEl = element.getElementsByClassName(`animation${index}`).item(0);
+    if (music_index == index) {
+      let playing_animation = `<div class="playing_animation"><span></span><span></span><span></span></div>`;
+      animationEl.innerHTML = playing_animation;
+    }
+    else {
+      animationEl.innerHTML = `<div>${number}</div>`;
+    }
+  });
+}
+
+const hideSongsList = () => {
+  songsListContainer.style.transform = "translateY(100%)";
+}
+
+const showSongsList = () => {
+  songsListContainer.style.transform = "translateY(0)";
+}
+
+const renderModeIcon = () => {
+  modeButton.getElementsByTagName("span").item(0).innerHTML = modes[mode_index];
+  songsListModeButton.getElementsByTagName("span").item(0).innerHTML = modes[mode_index];
+}
+
+const renderPlayIcon = () => {
+  if (pasused) {
+    playButton.getElementsByTagName("span").item(0).innerHTML = "play_arrow";
+  } else {
+    playButton.getElementsByTagName("span").item(0).innerHTML = "pause";
+  }
+}
+
+const playAudio = () => {
+  audio.play();
+}
+
+const pauseAudio = () => {
+  audio.pause();
+}
+
+const loadMusic = () => {
+  let song = songs[music_index];
+
+  audio.setAttribute("src", song.audioDir);
+  title.textContent = song.title;
+  artist.textContent = song.artist;
+  cover.setAttribute("src", song.cover);
+
+  musicContainer.style.backgroundImage = `url(${song.cover})`;
+}
+
+const liveMusic = () => {
+  (pasused) ? pauseAudio() : playAudio();
+}
+
+const playMusic = (index) => {
+  music_index = index;
+  loadMusic();
+  setPause(false);
+  liveMusic();
+  renderPlayIcon();
+  hideSongsList();
+  loadPlayingSongInList();
+}
+
+const previousMusic = () => {
+  music_index -= 1;
+  (music_index < 0) ? music_index = songs.length - 1 : mode_index = music_index ;
+  loadMusic();
+  setPause(false);
+  liveMusic();
+  renderPlayIcon();
+}
+
+const nextMusic = () => {
+  music_index += 1;
+  (music_index > songs.length - 1) ? music_index = 0 : mode_index = music_index;
+  loadMusic();
+  setPause(false);
+  liveMusic();
+  renderPlayIcon();
+}
+
+const setPause = (what_ever) => {
+  pasused = what_ever;
+}
+
+const changeMode = () => {
+  mode_index += 1;
+  if (mode_index > modes.length - 1) {
+    mode_index = 0;
+  }
+
+  let label = "";
+  switch (modes[mode_index]) {
+    case "repeat":
+      label = "Repeat All";
+      break;
+
+    case "repeat_one":
+      label = "Repeat One";
+      break;
+
+    case "shuffle":
+      label = "Shuffle";
+      break;
+  }
+  songsListModeName.textContent = label;
+  renderModeIcon();
+}
+
+modeButton.addEventListener('click', () => {
+  changeMode();
+});
+
+songsListModeButton.addEventListener('click', () => {
+  changeMode();
+});
+
+playButton.addEventListener('click', () => {
+  (pasused) ? pasused = false : pasused = true;
+  liveMusic();
+  renderPlayIcon();
+  loadPlayingSongInList();
+});
+
+previousButton.addEventListener('click', () => {
+  previousMusic();
+});
+
+nextButton.addEventListener('click', () => {
+  nextMusic();
+});
+
+listButton.addEventListener('click', () => {
+  showSongsList();
+});
+
+songsListCloseButton.addEventListener('click', () => {
+  hideSongsList();
+});
+
+prgressArea.addEventListener('click', (event) => {
+  audio.currentTime = (event.offsetX / prgressArea.clientWidth) * audio.duration;
+  setPause(false);
+  liveMusic();
+  renderPlayIcon();
+});
+
+audio.addEventListener('timeupdate', (event) => {
+  let current_time = event.target.currentTime;
+  let total_time = event.target.duration;
+
+  let progress_width = (current_time / total_time) * 100;
+  progressBar.style.width = progress_width + "%";
+
+  let current_min = Math.floor(current_time / 60);
+  let current_sec = Math.floor(current_time % 60);
+  if (current_sec < 10) {
+    current_sec = '0' + current_sec;
+  }
+  currentTime.textContent = `${current_min}:${current_sec}`;
+});
+
+audio.addEventListener('loadeddata', (event) => {
+  let total_time = event.target.duration;
+  let total_min = Math.floor(total_time / 60);
+  let total_sec = Math.floor(total_time % 60);
+  if (total_sec < 10) {
+    total_sec = '0' + total_sec;
+  }
+  duration.textContent = `${total_min}:${total_sec}`;
+});
+
+audio.addEventListener('ended', () => {
+  switch (modes[mode_index]) {
+    case "repeat":
+      nextMusic();
+      break;
+
+    case "repeat_one":
+      audio.currentTime = 0;
+      playAudio();
+      break;
+
+    case "shuffle":
+      while (true) {
+        let random_index = Math.floor(Math.random() * songs.length);
+        if (random_index !== music_index) {
+          music_index = random_index;
+          loadMusic();
+          playAudio();
+          break;
+        }
+      }
+      break;
+  }
+});
+
+window.addEventListener('load', () => {
+  setMusicContainerCenter();
+  renderPlayIcon();
+  renderModeIcon();
+  loadMusic();
+  loadSongsList();
+});
